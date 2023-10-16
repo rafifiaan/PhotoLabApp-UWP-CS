b424bcbcb78e803079354e5e348e073301c8bf60
rafifiaan <rafialiefian03@gmail.com>
2023-10-16 22:21:55 +0700

    build(release): a project src and documentation/note

---
+++ PhotoLab/MainPage.xaml.cs
@@ -0,0 +1,289 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.Storage.Streams;
+using Windows.System.Profile;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace PhotoLab
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public static MainPage Current;
+        private ImageFileInfo persistedItem;
+
+        public ObservableCollection<ImageFileInfo> Images { get; } = new ObservableCollection<ImageFileInfo>();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Current = this;
+        }
+
+        // If the image is edited and saved in the details page, this method gets called
+        // so that the back navigation connected animation uses the correct image.
+        public void UpdatePersistedItem(ImageFileInfo item)
+        {
+            persistedItem = item;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                AppViewBackButtonVisibility.Collapsed;
+
+            if (Images.Count == 0)
+            {
+                await GetItemsAsync();
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+
+
+
+
+        private async void TambahFoto_Click(object sender, RoutedEventArgs e)
+        {
+            // Tampilkan dialog pemilihan file atau kamera
+            var filePicker = new Windows.Storage.Pickers.FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".jpg");
+            filePicker.FileTypeFilter.Add(".png");
+            filePicker.FileTypeFilter.Add(".gif");
+
+            var selectedFile = await filePicker.PickSingleFileAsync();
+
+            if (selectedFile != null)
+            {
+                // Simpan foto yang dipilih di local folder aplikasi
+                await SavePhotoToLocalFolder(selectedFile);
+            }
+        }
+
+        private async Task SavePhotoToLocalFolder(StorageFile photoFile)
+        {
+            try
+            {
+                // Dapatkan local folder aplikasi
+                StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+
+                // Buat folder "Samples" jika belum ada
+                StorageFolder samplesFolder = await localFolder.CreateFolderAsync("Samples", CreationCollisionOption.OpenIfExists);
+
+                // Buat file target di dalam folder "Samples"
+                StorageFile targetFile = await samplesFolder.CreateFileAsync(photoFile.Name, CreationCollisionOption.GenerateUniqueName);
+
+                // Salin file foto ke folder target
+                await photoFile.CopyAndReplaceAsync(targetFile);
+
+                // Proses foto yang disalin dan tambahkan ke koleksi
+                Images.Add(await LoadImageInfo(targetFile));
+
+#if DEBUG
+                // Hanya pada mode Debug, salin juga ke folder "Assets/Samples" untuk pengembangan
+                StorageFolder assetsFolder = await Package.Current.InstalledLocation.GetFolderAsync("Assets");
+                StorageFolder debugSamplesFolder = await assetsFolder.CreateFolderAsync("Samples", CreationCollisionOption.OpenIfExists);
+                await photoFile.CopyAsync(debugSamplesFolder, photoFile.Name, NameCollisionOption.GenerateUniqueName);
+#endif
+            }
+            catch (Exception ex)
+            {
+                // Tangani kesalahan jika ada
+                // Misalnya, tampilkan pesan kesalahan
+                var dialog = new MessageDialog($"Error: {ex.Message}");
+                await dialog.ShowAsync();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        // Called by the Loaded event of the ImageGridView.
+        private async void StartConnectedAnimationForBackNavigation()
+        {
+            // Run the connected animation for navigation back to the main page from the detail page.
+            if (persistedItem != null)
+            {
+                ImageGridView.ScrollIntoView(persistedItem);
+                ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("backAnimation");
+                if (animation != null)
+                {
+                    await ImageGridView.TryStartConnectedAnimationAsync(animation, persistedItem, "ItemImage");
+                }
+            }
+        }
+
+        private void ImageGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Prepare the connected animation for navigation to the detail page.
+            persistedItem = e.ClickedItem as ImageFileInfo;
+            ImageGridView.PrepareConnectedAnimation("itemAnimation", e.ClickedItem, "ItemImage");
+
+            this.Frame.Navigate(typeof(DetailPage), e.ClickedItem);
+        }
+
+        private async Task GetItemsAsync()
+        {
+            QueryOptions options = new QueryOptions();
+            options.FolderDepth = FolderDepth.Deep;
+            options.FileTypeFilter.Add(".jpg");
+            options.FileTypeFilter.Add(".png");
+            options.FileTypeFilter.Add(".gif");
+
+            // Get the Pictures library. (Requires 'Pictures Library' capability.)
+            //Windows.Storage.StorageFolder picturesFolder = Windows.Storage.KnownFolders.PicturesLibrary;
+            // OR
+            // Get the Sample pictures.
+            StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
+            StorageFolder picturesFolder = await appInstalledFolder.GetFolderAsync("Assets\\Samples");
+
+            var result = picturesFolder.CreateFileQueryWithOptions(options);
+
+            IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
+            bool unsupportedFilesFound = false;
+            foreach (StorageFile file in imageFiles)
+            {
+                // Only files on the local computer are supported. 
+                // Files on OneDrive or a network location are excluded.
+                if (file.Provider.Id == "computer")
+                {
+                    Images.Add(await LoadImageInfo(file));
+                }
+                else
+                {
+                    unsupportedFilesFound = true;
+                }
+            }
+
+            if (unsupportedFilesFound == true)
+            {
+                ContentDialog unsupportedFilesDialog = new ContentDialog
+                {
+                    Title = "Unsupported images found",
+                    Content = "This sample app only supports images stored locally on the computer. We found files in your library that are stored in OneDrive or another network location. We didn't load those images.",
+                    CloseButtonText = "Ok"
+                };
+
+                ContentDialogResult resultNotUsed = await unsupportedFilesDialog.ShowAsync();
+            }
+        }
+
+        public async static Task<ImageFileInfo> LoadImageInfo(StorageFile file)
+        {
+            var properties = await file.Properties.GetImagePropertiesAsync();
+            ImageFileInfo info = new ImageFileInfo(
+                properties, file,
+                file.DisplayName, file.DisplayType);
+
+            return info;
+        }
+
+        public double ItemSize
+        {
+            get => _itemSize;
+            set
+            {
+                if (_itemSize != value)
+                {
+                    _itemSize = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ItemSize)));
+                }
+            }
+        }
+        private double _itemSize;
+
+        private void DetermineItemSize()
+        {
+            if (FitScreenToggle != null
+                && FitScreenToggle.IsOn == true
+                && ImageGridView != null
+                && ZoomSlider != null)
+            {
+                // The 'margins' value represents the total of the margins around the
+                // image in the grid item. 8 from the ItemTemplate root grid + 8 from
+                // the ItemContainerStyle * (Right + Left). If those values change,
+                // this value needs to be updated to match.
+                int margins = (int)this.Resources["LargeItemMarginValue"] * 4;
+                double gridWidth = ImageGridView.ActualWidth - (int)this.Resources["DefaultWindowSidePaddingValue"];
+                double ItemWidth = ZoomSlider.Value + margins;
+                // We need at least 1 column.
+                int columns = (int)Math.Max(gridWidth / ItemWidth, 1);
+
+                // Adjust the available grid width to account for margins around each item.
+                double adjustedGridWidth = gridWidth - (columns * margins);
+
+                ItemSize = (adjustedGridWidth / columns);
+            }
+            else
+            {
+                ItemSize = ZoomSlider.Value;
+            }
+        }
+
+        private void ImageGridView_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.InRecycleQueue)
+            {
+                var templateRoot = args.ItemContainer.ContentTemplateRoot as Grid;
+                var image = (Image)templateRoot.FindName("ItemImage");
+
+                image.Source = null;
+            }
+
+            if (args.Phase == 0)
+            {
+                args.RegisterUpdateCallback(ShowImage);
+                args.Handled = true;
+            }
+        }
+
+        private async void ShowImage(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.Phase == 1)
+            {
+                // It's phase 1, so show this item's image.
+                var templateRoot = args.ItemContainer.ContentTemplateRoot as Grid;
+                var image = (Image)templateRoot.FindName("ItemImage");
+                image.Opacity = 100;
+
+                var item = args.Item as ImageFileInfo;
+
+                try
+                {
+                    image.Source = await item.GetImageThumbnailAsync();
+                }
+                catch (Exception)
+                {
+                    // File could be corrupt, or it might have an image file
+                    // extension, but not really be an image file.
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.UriSource = new Uri(image.BaseUri, "Assets/StoreLogo.png");
+                    image.Source = bitmapImage;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# PhotoLabApp-UWP-CS
+## Photo Lab App on UWP using .NET Framework & C# Language
+Created with `Visual Studio 2022` & `Universal Windows Platform` (UWP) Templates
+
+- User Interface :
+
+<img src="img/UI-PhotoLab.png" alt="UI-PhotoLab" width="670" height="450">
+
+</br>
+
+*Thank you for visiting this repository, stay tuned for more projects!*
